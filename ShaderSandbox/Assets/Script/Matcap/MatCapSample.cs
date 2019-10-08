a5065d2382737d13fe49a5d9c2ea2e97c42282a0
Cova8bitdot <cova.tech.8bitdots@gmail.com>
2019-10-08 14:43:15 +0900

    Unityプロジェクト&MatCap のサンプル作成

---
+++ ShaderSandbox/Assets/Script/Matcap/MatCapSample.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace cova.tech.sandbox.shader
+{
+	public class MatCapSample : MonoBehaviour
+	{
+		[SerializeField]
+		Material TargetMaterial = null;
+	
+	
+		private void OnRenderImage(RenderTexture src, RenderTexture dest)
+		{
+			Graphics.Blit( src, dest, TargetMaterial );
+		}
+	}
+
+}
